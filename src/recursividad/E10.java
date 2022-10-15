201ec54d4d6e16c276c06b91d2d45c24c75d5962
Coms-Lap <Coms-Lap@DESKTOP-MQ9M3QT>
2022-10-15 09:18:07 -0500

    EJERCICIOS DE RECURSIVIDAD

---
+++ src/recursividad/E10.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursividad;
+
+/**
+ *
+ * @author MOISES OSCCO
+ */
+public class E10 {
+    public static void main(String[] args)
+{
+    //palabra analizada
+    String palabra="HOLA";
+    if(orden(palabra)) System.out.println("La palabra esta ordenada alfabeticamente");
+    else System.out.println("La palabra NO esta ordenada alfabeticamente");
+}
+//método que comprueba si una palabra está ordenada alfabéticamente
+//ejemplo: abcdef -> true
+public static boolean orden(String cad)
+{
+    cad=cad.toLowerCase();
+    if(cad.length()>1)
+    {
+        if(cad.charAt(0)<=cad.charAt(1))
+        return orden(cad.substring(1, cad.length()));
+        else return false;
+    }
+    else return true;
+}
+    
+}
